e03473c1bf0c1803a5332eab5efddc697e4e218e
Cuong Nguyen <cuongnguyen29091997@gmail.com>
2020-06-09 08:29:12 +0700

    add user

---
+++ BookStore/Migrations/20200609012432_InsertUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookStore.Migrations
+{
+    public partial class InsertUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData("userdetails", new string[] {"id", "Name", "email","password"}, new object[,]
+            {
+                {1, "Cường Nguyễn", "quoccuong@gmail.com", "123456" },
+                {2, "Cường Nguyễn Quốc", "quoccuong1234@gmail.com", "123456" },
+                {3, "Nguyễn Quốc Cường", "quoccuong123@gmail.com", "123456" }
+            });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
